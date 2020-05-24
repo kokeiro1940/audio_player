e131f1345de56953058fd6c61b2989ea08bf4615
Atsuki-Tonomura <tonomura@mfeed.ad.jp>
2020-05-24 16:20:14 +0900

    基本的な機能実装

--- audio.js
+++ audio.js
@@ -2,7 +2,7 @@ var music = new Audio();
 
 function init() {
   music.preload = "auto";
-  music.src = "./music/05 運命線上のアリア.mp3";
+  music.src = "./music/shining star.mp3";
   music.load();
 
   music.addEventListener("ended", function () {
@@ -25,17 +25,6 @@ function repeat() {
 // 一時停止
 function pause() {
     music.pause();
-}
-
-// 曲送り
-function next() {
-  load("./music/Rally Go Round.mp3");
-}
-
-// 曲読み込み
-function load(music_path) {
-  music.src = music_path;
-  music.load();
 }
 
 init();
