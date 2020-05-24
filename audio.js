ee7a153762e91f52a5ccea7e8b99a39a27553e7e
Atsuki-Tonomura <tonomura@mfeed.ad.jp>
2020-05-24 13:35:46 +0900

    インデント削除

--- audio.js
+++ audio.js
@@ -1,31 +1,31 @@
-    var music = new Audio();
+var music = new Audio();
 
-    function init() {
-      music.preload = "auto";
-      music.src = "./music/05 運命線上のアリア.mp3";
-      music.load();
+function init() {
+  music.preload = "auto";
+  music.src = "./music/05 運命線上のアリア.mp3";
+  music.load();
 
-      music.addEventListener("ended", function () {
-        music.currentTime = 0;
-        music.play();
-      }, false);
-    }
+  music.addEventListener("ended", function () {
+    music.currentTime = 0;
+    music.play();
+  }, false);
+}
 
-    // 再生
-    function play() {
-      music.loop = true;
-      music.play();
-    }
+// 再生
+function play() {
+  music.loop = true;
+  music.play();
+}
 
-    // 巻き戻し停止
-    function stop() {
-      music.pause();
-      music.currentTime = 0;
-    }
+// 巻き戻し停止
+function stop() {
+  music.pause();
+  music.currentTime = 0;
+}
 
-    // 一時停止
-    function pause() {
-        music.pause();
-      }
+// 一時停止
+function pause() {
+    music.pause();
+  }
 
-    init();
+init();
